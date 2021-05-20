4d35e59c0856c6e40f2b9b6c70dc259ed2ed345b
Glitch (ai1-web) <none>
2021-05-20 11:44:54 +0000

    💍🌘 Checkpoint ./style.css:31964368/2660 ./index.html:31964368/366 ./script.js:31964368/327

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-$.backstretch("/img/winter.jpg");
+/* Toggle between adding and removing the "responsive" class to the navbar when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myNavbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
